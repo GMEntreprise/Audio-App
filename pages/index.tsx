2666ee976590b7d15fab3efea3a0390d7655b4ae
GMWebDevelopment <gmwebdevelopments@gmail.com>
2023-05-05 17:17:48 +0200

    🚀 : DEPLOY APP MUSIC

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,59 @@
-import { InferGetStaticPropsType } from 'next';
-import { NextPage } from 'next';
-import { useState } from 'react';
-import Song from '../components/Song/Song';
-import Audio from '../components/Audio/Audio'
-import styles from '../styles/Home.module.css'
+import { InferGetStaticPropsType } from "next";
+import { NextPage } from "next";
+import { useState } from "react";
+import Song from "../components/Song/Song";
+import Audio from "../components/Audio/Audio";
+import styles from "../styles/Home.module.css";
 
 const SONGS: Song[] = [
   {
     id: 0,
-    title: 'Birdseye Blues',
-    artist: 'Chris Haugen',
-    file: 'Birdseye Blues - Chris Haugen.mp3',
-    image: '/1.jpg'
+    title: "After All These Years",
+    artist: "BRIAN & JENN JOHNSON",
+    file: "songs/For the One -  Brian.mp3",
+    image: "/covers/after-these.jpeg",
   },
   {
     id: 1,
-    title: 'Depth Fuse',
-    artist: 'French Fuse',
-    file: 'Depth Fuse - French Fuse.mp3',
-    image: '/2.jpg'
+    title: "The Undoing",
+    artist: "STEFFANY GRETZINGER",
+    file: "songs/Out-of-Hiding.mp3",
+    image: "/covers/the-undoing.jpeg",
   },
-  {
-    id: 2,
-    title: 'Duh Fuse',
-    artist: 'French Fuse',
-    file: 'Duh Fuse - French Fuse.mp3',
-    image: '/3.jpg'
-  }
-]
+];
 
-export const getStaticProps = async() => {
+export const getStaticProps = async () => {
   const allSongs: Song[] = SONGS;
   return {
     props: {
-      songs: allSongs
+      songs: allSongs,
     },
-    revalidate: 3600
-  }
-}
+    revalidate: 3600,
+  };
+};
 
-const Home: NextPage<{ songs: Song[]}> = ({ songs }) => {
-
-  const [trackPlaying, setTrackPlaying] = useState<number>(0)
+const Home: NextPage<{ songs: Song[] }> = ({ songs }) => {
+  const [trackPlaying, setTrackPlaying] = useState<number>(0);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   return (
     <div className={styles.container}>
       <div className={styles.songPlaying}>
-        <Song isPlaying={isPlaying} trackPlaying={trackPlaying} song={songs[trackPlaying]} />
+        <Song
+          isPlaying={isPlaying}
+          trackPlaying={trackPlaying}
+          song={songs[trackPlaying]}
+        />
       </div>
-      <Audio isPlaying={isPlaying} setIsPlaying={setIsPlaying} songs={songs} trackPlaying={trackPlaying} setTrackPlaying={setTrackPlaying} />
+      <Audio
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        songs={songs}
+        trackPlaying={trackPlaying}
+        setTrackPlaying={setTrackPlaying}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
